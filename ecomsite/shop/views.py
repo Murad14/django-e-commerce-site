5869c1b3d7ac0342521a9912ca7dfc85e5b75575
Murad <49407545+Murad14@users.noreply.github.com>
2023-06-19 21:29:16 +0600

    search feature implemented

--- ecomsite/shop/views.py
+++ ecomsite/shop/views.py
@@ -4,4 +4,9 @@ from .models import Products
 
 def index(req):
     product_objects = Products.objects.all()
+    
+    item_name = req.GET.get('item_name')
+    if item_name != '' and item_name is not None:
+        product_objects = product_objects.filter(title__icontains= item_name)
+    
     return render(req,'shop/index.html',{'product_objects':product_objects})
