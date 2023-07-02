5d4748bc5b118ca20633a8e64dcfffbbfc6c3b12
Murad <49407545+Murad14@users.noreply.github.com>
2023-07-03 00:37:48 +0600

    admin_panel modified

--- ecomsite/shop/admin.py
+++ ecomsite/shop/admin.py
@@ -2,5 +2,13 @@ from django.contrib import admin
 from .models import Products, Order
 # Register your models here.
 
-admin.site.register(Products)
+admin.site.site_header = "E-commerce site"
+admin.site.site_title = "Shopno"
+admin.site.index_title = "Manage Shopping"
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('title','price','discount_price','category','description')
+    
+
+admin.site.register(Products,ProductAdmin)
 admin.site.register(Order)
