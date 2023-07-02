6130d04532802348c375b40f8c19854aed1d119d
Murad <49407545+Murad14@users.noreply.github.com>
2023-07-03 01:55:21 +0600

    admin_panel_customized

--- ecomsite/shop/admin.py
+++ ecomsite/shop/admin.py
@@ -6,8 +6,21 @@ admin.site.site_header = "E-commerce site"
 admin.site.site_title = "Shopno"
 admin.site.index_title = "Manage Shopping"
 
+
+
 class ProductAdmin(admin.ModelAdmin):
+    
+    def change_catgory_to_default(self,req,queryset):
+        queryset.update(category="default")
+        
+    change_catgory_to_default.short_description ='Default Category'
     list_display = ('title','price','discount_price','category','description')
+    search_fields = ['title','category','price','discount_price']
+    actions = ('change_catgory_to_default',)
+    fields = ('title','price',)
+    list_editable = ('price','category',)
+    
+    
     
 
 admin.site.register(Products,ProductAdmin)
